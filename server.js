cae7c2a042670cff225717aebd829bfe629ca2c8
underthenightsky <josephkurianjohn_ee22b16_57@dtu.ac.in>
2025-07-09 19:23:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const   app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:'*'
+    origin:'https://react-notes-frontend-gamma.vercel.app/'
 }))
 app.use('/notes', notesRoutes);
 // here we are choosing the port on which the backend is running , on which site it should listen to requests to 
