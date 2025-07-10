89774bb871ab6cb10934d6022945b594b97326e6
underthenightsky <josephkurianjohn_ee22b16_57@dtu.ac.in>
2025-07-10 15:57:42 +0530

    k

--- server.js
+++ server.js
@@ -1,44 +1,42 @@
 // to handle api requests
-import 'dotenv/config'
-import  express from 'express';
-import  cors from 'cors';
-import  notesRoutes from './routes/notesRoutes.js';
-import  {connectDB} from "./lib/connectDb.js";
-const   app = express();
-
-
-
-app.use(cors(
-    {origin:['https://react-notes-frontend-gamma.vercel.app',"http://localhost:5173"],
-        methods: ["POST", "GET","DELETE"],
-        credentials:true
-    }
-    ))
-    app.use(express.json());
-app.use('/notes', notesRoutes);
-// here we are choosing the port on which the backend is running , on which site it should listen to requests to 
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./lib/connectDb.js";
+const app = express();
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path"
+app.use(
+  cors({
+    origin: [
+      "https://react-notes-frontend-gamma.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/notes", notesRoutes);
+// here we are choosing the port on which the backend is running , on which site it should listen to requests to
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../React_Notes_Frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../React_Notes_Frontend", "dist", "index.html")
+    );
+  });
+}
 const PORT = 5000;
-app.get("/" ,(req,res)=>{
-    return res.json({"message":"Welcome"})
-})
-// export async function OPTIONS() {
-//   return new Response(null, {
-//     status: 200,
-//     headers: {
-//       'Access-Control-Allow-Origin':"*",
-//       'Access-Control-Allow-Methods': 'GET, POST,DELETE, OPTIONS',
-//       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//       'Access-Control-Allow-Credentials': 'true',
-//     },
-//   });
-// }
-// export async function GET() {
-//   return Response.json({ ok: true }, {
-//     headers: { 'Access-Control-Allow-Origin': '*' },
-//   });
-// }
-app.listen(5000, async() =>
-    {  await connectDB()
-        console.log(`Server running on port ${PORT}`)
-        
-    });
+app.get("/", (req, res) => {
+  return res.json({ message: "Welcome" });
+});
+
+app.listen(5000, async () => {
+  await connectDB();
+  console.log(`Server running on port ${PORT}`);
+});
